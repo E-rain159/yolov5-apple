7d89fca717713d5f98c2f5f81981da91c8da95c1
金智勋 <13944319585@163.com>
2026-06-21 13:13:00 +0800

    Initial commit

---
+++ classify/download.py
@@ -0,0 +1,8 @@
+
+
+from roboflow import Roboflow
+rf = Roboflow(api_key="DPkhFhdDgqAlYOMV4aPO")
+project = rf.workspace("jeeh").project("apple-f3ujh-r3zdl")
+version = project.version(1)
+dataset = version.download("yolov5")
+                
